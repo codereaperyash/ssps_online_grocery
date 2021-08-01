f9f9eda595957b63a1d2e022936862d5ec85d373
codereaperyash <therelaxingextreme@gmail.com>
2021-08-01 13:00:13 +0530

    1

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://codereaper-backend-ssps.herokuapp.com/api/v1/'
+  apiUrl: 'https://codereaper-backend-ssps.herokuapp.com/api/v1/'
 };
--- environments/environment.ts
+++ environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'http://codereaper-backend-ssps.herokuapp.com/api/v1/'
+    apiUrl: 'https://codereaper-backend-ssps.herokuapp.com/api/v1/'
   };
   
   /*
